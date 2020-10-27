a9c4eee429e751c64dfa78815ddc1da5cf0a5c21
Juan Pablo <juampigarcia2014@gmail.com>
2020-10-27 19:09:46 -0300

    Angular sesions working

--- seismology/web-angular/src/app/app.component.ts
+++ seismology/web-angular/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AuthService {
   title = 'web-angular';
+
+  constructor(private authService: AuthService) { }
+
+  logout() {
+    this.authService.logout()
+  }
 }
