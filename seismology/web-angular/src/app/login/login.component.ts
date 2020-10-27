02e96282feae62a4d291b179bf839948a1eaa0ab
Juan Pablo <juampigarcia2014@gmail.com>
2020-10-27 18:51:45 -0300

    Prepare login

--- seismology/web-angular/src/app/login/login.component.ts
+++ seismology/web-angular/src/app/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+//Importar servicio de Auth
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email = '';
+  password = '';
+
+  constructor(private authService: AuthService) { }
+
+  //Llama a la funcion de login del servicio
+  login() {
+    this.authService.login(this.email, this.password)
+  }
 
   ngOnInit(): void {
   }
